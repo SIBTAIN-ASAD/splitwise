6e12852645cd1135c96d4a5ce0fad9350d966c8d
Muhammad Sibtain Asad <sibtain.asad@devsinc.com>
2024-02-13 17:18:10 +0500

    completed : project - functional points of projects are completed

--- src/components/buttons/LoginButton.jsx
+++ src/components/buttons/LoginButton.jsx
@@ -1,73 +1,84 @@
 import React from 'react';
+import { useState } from 'react';
 import { useAuth } from '../AuthContext';
 import { addDoc, collection, getDocs } from 'firebase/firestore';
-import db  from '../../config/firebasedb';
+import db from '../../config/firebasedb';
 import { useNavigate } from 'react-router-dom';
 import GoogleButton from './GoogleButton';
 import LogoutIcon from '../icons/LogoutIcon';
 import { getAuth } from 'firebase/auth';
+import LoadingOverlay from '../loading/LoadingOverlay';
 
 const LoginButton = () => {
   const { currentUser, signInWithGoogle, signOutUser } = useAuth();
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState(false);
 
   const registerWithGoogle = async () => {
-      const auth = getAuth();
-      let { displayName, email, photoURL: imageUrl } = auth.currentUser;
+    setLoading(true);
+    const auth = getAuth();
+    let { displayName, email, photoURL: imageUrl } = auth.currentUser;
 
-      if (!imageUrl) {
-          imageUrl = 'https://via.placeholder.com/250';
+    if (!imageUrl) {
+      imageUrl = 'https://via.placeholder.com/250';
+    }
+
+    try {
+      const docRef = collection(db, 'user_detail');
+      let found = false;
+      // check if email already exists
+      const querySnapshot = await getDocs(docRef);
+      querySnapshot.forEach((doc) => {
+        if (doc.data().email === email) {
+          found = true;
+        }
+      });
+
+      if (found) {
+        return;
       }
-      
-      try {
-          const docRef = collection(db, 'user_detail');
-          let found = false;
-          // check if email already exists
-          const querySnapshot = await getDocs(docRef);
-          querySnapshot.forEach((doc) => {
-              if (doc.data().email === email) {
-                  found = true;
-              }
-          });
 
-          if (found) {
-              return;
-          }
-
-          await addDoc(docRef, {
-              displayName,
-              email,
-              picture: imageUrl,
-              userName: displayName,
-          });
-          navigate('/');
-      } catch (error) {
-          console.error('Error adding user:', error.message);
-      }
+      await addDoc(docRef, {
+        displayName,
+        email,
+        picture: imageUrl,
+        userName: displayName,
+      });
+      setLoading(false);
+      navigate('/');
+    } catch (error) {
+      console.error('Error adding user:', error.message);
+      setLoading(false);
+    }
   }
 
   const handleGoogleSignIn = async () => {
-      try {
-          await signInWithGoogle();
-          await registerWithGoogle();
-      } catch (error) {
-          console.error('Error signing in with Google:', error.message);
-      }
+    try {
+      setLoading(true);
+      await signInWithGoogle();
+      await registerWithGoogle();
+      setLoading(false);
+    } catch (error) {
+      console.error('Error signing in with Google:', error.message);
+      setLoading(false);
+    }
   };
 
 
   return (
-    <div className="flex items-center space-x-2">
-      {currentUser ? (
-        <button type="button" onClick={signOutUser}>
-          <LogoutIcon />
-          <span className="sr-only">Logout</span>
-        </button>
-      ) : (
-        <GoogleButton onClick={handleGoogleSignIn} />
-      )}
-    </div>
+    <>
+      {loading && <LoadingOverlay />}
+      <div className="flex items-center space-x-2">
+        {currentUser ? (
+          <button type="button" onClick={signOutUser}>
+            <LogoutIcon />
+            <span className="sr-only">Logout</span>
+          </button>
+        ) : (
+          <GoogleButton onClick={handleGoogleSignIn} />
+        )}
+      </div>
+    </>
   );
 };
 
